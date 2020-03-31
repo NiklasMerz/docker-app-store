816a26a0ae894346cbcd8e1dd5e05dc1aa47aa84
Niklas Merz <niklasmerz@linux.com>
2020-03-31 20:45:17 +0200

    Create home and store page

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { HomePageRoutingModule } from './store-routing.module';
 
-import { FolderPage } from './folder.page';
+import { FolderPage as StorePage } from './store.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    HomePageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [StorePage]
 })
-export class FolderPageModule {}
+export class StorePageModule {}
